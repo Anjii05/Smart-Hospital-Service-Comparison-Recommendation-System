4820bf136c5463e53f52c27741f4cefe8ffe33a6
Anjali Kodolli <kodollianjali@gmail.com>
2026-04-08 16:53:53 +0530

    Fixed hospital filtering bug

--- hospital-project/backend/config/database.js
+++ hospital-project/backend/config/database.js
@@ -1,27 +1,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DATABASE_URL ,
-  user: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD ,
-  database: process.env.DB_NAME ,
-  port:29643,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  timezone: '+05:30',
-});
+const pool = mysql.createPool(
+  process.env.DATABASE_URL);
 
-async function testConnection() {
-  try {
-    const conn = await pool.getConnection();
-    console.log('✅ MySQL connected successfully');
-    conn.release();
-  } catch (err) {
-    console.error('❌ MySQL connection failed:', err.message);
-    process.exit(1);
-  }
-}
-
-module.exports = { pool, testConnection };
+module.exports =  pool;
