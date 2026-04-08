e652431a0b2b2d93e0caac8171d30be97b58d5a3
Anjali Kodolli <kodollianjali@gmail.com>
2026-04-08 16:45:04 +0530

    Fixed hospital filtering bug

--- hospital-project/backend/config/database.js
+++ hospital-project/backend/config/database.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Anjali123',
-  database: process.env.DB_NAME || 'hospital_finder',
-  port: parseInt(process.env.DB_PORT) || 3306,
+  host: process.env.DATABASE_URL ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
+  port:29643,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
